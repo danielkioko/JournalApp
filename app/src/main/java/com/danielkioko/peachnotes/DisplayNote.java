1a7394845a41e6612f508cc93e55b4c05900f3c2
danielnzioka <danielkioko.998@gmail.com>
2019-01-31 09:26:38 -0500

    updates

--- app/src/main/java/com/danielkioko/peachnotes/DisplayNote.java
+++ app/src/main/java/com/danielkioko/peachnotes/DisplayNote.java
@@ -4,18 +4,33 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import android.widget.Toolbar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -25,12 +40,26 @@ public class DisplayNote extends AppCompatActivity {
     private NDb mydb;
     EditText name;
     EditText content;
+
+    Button saveNote;
+
     private CoordinatorLayout coordinatorLayout;
     String dateString;
     Bundle extras;
+
     int id_To_Update = 0;
+
     Snackbar snackbar;
     Toolbar toolbar;
+
+    private FirebaseDatabase database;
+    private StorageReference storage;
+    private DatabaseReference databaseRef;
+    private DatabaseReference mDatabaseUsers;
+    private FirebaseUser mCurrentUser;
+    private FirebaseAuth mAuth;
+
+    private Uri uri = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +68,14 @@ public class DisplayNote extends AppCompatActivity {
         setContentView(R.layout.viewnotepad);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        storage = FirebaseStorage.getInstance().getReference();
+        databaseRef = FirebaseDatabase.getInstance().getReference().child("Notes");
+        mAuth = FirebaseAuth.getInstance();
+        mCurrentUser = mAuth.getCurrentUser();
+        mDatabaseUsers = FirebaseDatabase.getInstance().getReference().child("Users").child(mCurrentUser.getUid());
+
+//        String noteID = getIntent().getExtras().getString("noteID");
 
         name = findViewById(R.id.txtname);
         Typeface typefaceTitle = Typeface.createFromAsset(getAssets(), "JosefinSans-Bold.ttf");
@@ -49,26 +86,100 @@ public class DisplayNote extends AppCompatActivity {
 
         coordinatorLayout = findViewById(R.id
                 .coordinatorLayout);
-        mydb = new NDb(this);
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            int Value = extras.getInt("id");
-            if (Value > 0) {
-                snackbar = Snackbar
-                        .make(coordinatorLayout, "Note Id : " + String.valueOf(Value), Snackbar.LENGTH_LONG);
-                snackbar.show();
-                Cursor rs = mydb.getData(Value);
-                id_To_Update = Value;
-                rs.moveToFirst();
-                String nam = rs.getString(rs.getColumnIndex(NDb.name));
-                String contents = rs.getString(rs.getColumnIndex(NDb.remark));
-                if (!rs.isClosed()) {
-                    rs.close();
-                }
-                name.setText(nam);
-                content.setText(contents);
+
+        saveNote = findViewById(R.id.btnSave);
+        saveNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final String title = name.getText().toString().trim();
+                final String notes = content.getText().toString().trim();
+
+                Calendar calendar = Calendar.getInstance();
+                System.out.println("Current time => " + calendar.getTime());
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MMM-yyyy");
+                String date = simpleDateFormat.format(calendar.getTime());
+                dateString = date;
+
+                databaseRef = FirebaseDatabase.getInstance().getReference();
+                Noter noter = new Noter(title, notes, date);
+                databaseRef.child("note_data").setValue(noter).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(DisplayNote.this, MyNotes.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Error!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+//                final StorageReference storageReference = storage.child("note_data").child(uri.getLastPathSegment());
+//                storageReference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//                    @Override
+//                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//
+//                        storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+//                            @Override
+//                            public void onSuccess(Uri uri) {
+//
+//                                Toast.makeText(getApplicationContext(), "Saved", Toast.LENGTH_LONG).show();
+//                                final DatabaseReference newNote = databaseRef.push();
+//
+//                                mDatabaseUsers.addValueEventListener(new ValueEventListener() {
+//                                    @Override
+//                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                                        newNote.child("title").setValue(title);
+//                                        newNote.child("notes").setValue(notes);
+//                                        newNote.child("time").setValue(dataSnapshot.child("time").getValue()).addOnCompleteListener(new OnCompleteListener<Void>() {
+//                                            @Override
+//                                            public void onComplete(@NonNull Task<Void> task) {
+//                                                if (task.isSuccessful()){
+//                                                    Intent intent = new Intent(DisplayNote.this, MyNotes.class);
+//                                                    startActivity(intent);
+//                                                } else {
+//                                                }
+//                                            }
+//                                        });
+//                                    }
+//
+//                                    @Override
+//                                    public void onCancelled(@NonNull DatabaseError databaseError) {
+//                                        Toast.makeText(getApplicationContext(), "Error! Locally Saved Instead", Toast.LENGTH_LONG).show();
+//                                    }
+//                                });
+//                            }
+//                        });
+//                    }
+//                });
+//
             }
-        }
+        });
+
+        databaseRef.child("notes").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                String noteTitle = (String) dataSnapshot.child("title").getValue();
+                String noteContent = (String) dataSnapshot.child("notes").getValue();
+
+                if (noteTitle == null && noteContent == null) {
+                    name.setText("");
+                    content.setText("");
+                } else {
+                    name.setText(noteTitle);
+                    content.setText(noteContent);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getApplicationContext(), "Oops! Something Happened, Close the app and try again", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
         @Override
         public boolean onCreateOptionsMenu(Menu menu) {
@@ -82,21 +193,27 @@ public class DisplayNote extends AppCompatActivity {
 
     public boolean onOptionsItemSelected(MenuItem item) {
             super.onOptionsItemSelected(item);
-            switch (item.getItemId()) {
-                case R.id.Delete:
+
+        switch (item.getItemId()) {
+
+            case R.id.Delete:
                     AlertDialog.Builder builder = new AlertDialog.Builder(this);
                     builder.setMessage("Delete Note")
                             .setPositiveButton("YES",
                                     new DialogInterface.OnClickListener() {
                                         public void onClick(DialogInterface dialog,
                                                             int id) {
-                                            mydb.deleteNotes(id_To_Update);
+
+                                            String noteID = getIntent().getExtras().getString("noteID");
+                                            databaseRef.child(noteID).removeValue();
+
                                             Toast.makeText(DisplayNote.this, "Deleted Successfully",Toast.LENGTH_SHORT).show();
                                             Intent intent = new Intent(
                                                     getApplicationContext(),
                                                     MyNotes.class);
                                             startActivity(intent);
                                             finish();
+
                                         }
                                     })
                             .setNegativeButton("NO",
@@ -112,30 +229,28 @@ public class DisplayNote extends AppCompatActivity {
 
                 case R.id.Save:
                     Bundle extras = getIntent().getExtras();
-                    Calendar c = Calendar.getInstance();
-                    System.out.println("Current time => " + c.getTime());
-                    SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
-                    String formattedDate = df.format(c.getTime());
-                    dateString = formattedDate;
+
                     if (extras != null) {
                         int Value = extras.getInt("id");
                         if (Value > 0) {
                             if (content.getText().toString().trim().equals("")
                                     || name.getText().toString().trim().equals("")) {
+
                                 InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
                                 imm.hideSoftInputFromWindow(coordinatorLayout.getWindowToken(), 0);
+
                                 snackbar = Snackbar
                                         .make(coordinatorLayout, "Write Something, Anything In Mind?", Snackbar.LENGTH_LONG);
                                 snackbar.show();
+
                             } else {
                                 if (mydb.updateNotes(id_To_Update, name.getText()
                                         .toString(), dateString, content.getText()
                                         .toString())) {
+
                                     InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
                                     imm.hideSoftInputFromWindow(coordinatorLayout.getWindowToken(), 0);
-                                    snackbar = Snackbar
-                                            .make(coordinatorLayout, "Your note Updated Successfully!!!", Snackbar.LENGTH_LONG);
-                                    snackbar.show();
+
 
                                 } else {
                                     InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
@@ -156,9 +271,12 @@ public class DisplayNote extends AppCompatActivity {
                             } else {
                                 if (mydb.insertNotes(name.getText().toString(), dateString,
                                         content.getText().toString())) {
+
                                     InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
                                     imm.hideSoftInputFromWindow(coordinatorLayout.getWindowToken(), 0);
+
                                     Intent backToMyNotes = new Intent(DisplayNote.this, MyNotes.class);
+
                                     startActivity(backToMyNotes);
                                     Toast.makeText(DisplayNote.this, "Added Successfully", Toast.LENGTH_LONG).show();
                                 } else {
--- app/src/main/java/com/danielkioko/peachnotes/MyNotes.java
+++ app/src/main/java/com/danielkioko/peachnotes/MyNotes.java
@@ -3,122 +3,135 @@ package com.danielkioko.peachnotes;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AbsListView;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
-import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toolbar;
+
+import com.danielkioko.peachnotes.Accounts.LoginActivity;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MyNotes extends AppCompatActivity {
     private ListView obj;
     NDb mydb;
     ImageButton btnadd;
-    ListView mylist;
+
     Menu menu;
     AlertDialog.Builder alertDialogBuilder;
     AlertDialog alertDialog;
+
     Context context = this;
     CoordinatorLayout coordinatorLayout;
     SimpleCursorAdapter adapter;
+
     Snackbar snackbar;
     Toolbar toolbar;
-    Switch backupRestoreSwitch;
+
+    private RecyclerView recyclerView;
+
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notedisplay);
-        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
-        mydb = new NDb(this);
+        coordinatorLayout = findViewById(R.id.coordinatorLayout);
 
+        recyclerView = findViewById(R.id.noteRV);
 
-        btnadd = (ImageButton) findViewById(R.id.btnadd);
+        final RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Notes");
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                Intent intent = new Intent(MyNotes.this, LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        };
+
+        btnadd = findViewById(R.id.btnadd);
         btnadd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Bundle dataBundle = new Bundle();
-                dataBundle.putInt("id", 0);
-                Intent intent = new Intent(getApplicationContext(),
-                        DisplayNote.class);
-                intent.putExtras(dataBundle);
+                Intent intent = new Intent(MyNotes.this, DisplayNote.class);
                 startActivity(intent);
-                finish();
             }
         });
 
-
-
-
-
-        Cursor c = mydb.fetchAll();
-        String[] fieldNames = new String[] { NDb.name, NDb._id, NDb.dates, NDb.remark };
-        int[] display = new int[] { R.id.txtnamerow, R.id.txtidrow,
-                R.id.txtdate,R.id.txtremark };
-        adapter = new SimpleCursorAdapter(this, R.layout.listtemplate, c, fieldNames,
-                display, 0);
-
-        mylist = (ListView) findViewById(R.id.listView1);
-        mylist.setAdapter(adapter);
-        mylist.setOnItemClickListener(new OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-                                    long arg3) {
-                LinearLayout linearLayoutParent = (LinearLayout) arg1;
-                LinearLayout linearLayoutChild = (LinearLayout) linearLayoutParent
-                        .getChildAt(0);
-                TextView m = (TextView) linearLayoutChild.getChildAt(1);
-                Bundle dataBundle = new Bundle();
-                dataBundle.putInt("id",
-                        Integer.parseInt(m.getText().toString()));
-                Intent intent = new Intent(getApplicationContext(),
-                        DisplayNote.class);
-                intent.putExtras(dataBundle);
-                startActivity(intent);
-                finish();
-            }
-        });
-
-        mylist.setOnScrollListener(new AbsListView.OnScrollListener() {
-            @Override
-            public void onScrollStateChanged(AbsListView view, int scrollState) {
-
-                int btnPosY = btnadd.getScrollY();
-                if (scrollState == SCROLL_STATE_TOUCH_SCROLL) {
-                    btnadd.animate().cancel();
-                    btnadd.animate().translationYBy(150);
-                } else if (scrollState == SCROLL_STATE_FLING) {
-                    btnadd.animate().cancel();
-                    btnadd.animate().translationYBy(150);
-                } else {
-                    btnadd.animate().cancel();
-                    btnadd.animate().translationY(btnPosY);
-                }
-
-            }
-
-            @Override
-            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-
-            }
-        });
     }
 
     @Override
     public void onStart() {
         super.onStart();
+
+        FirebaseRecyclerAdapter<Noter, NoteViewHolder> FBRA = new FirebaseRecyclerAdapter<Noter, NoteViewHolder>(
+                Noter.class,
+                R.layout.listtemplate,
+                NoteViewHolder.class,
+                mDatabase
+        ) {
+            @Override
+            protected void populateViewHolder(NoteViewHolder viewHolder, Noter model, int position) {
+
+                final String noteID = getRef(position).getKey();
+
+                viewHolder.setTitle(model.getTitle());
+                viewHolder.setTitle(model.getTime());
+
+                viewHolder.view.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent singleActivity = new Intent(MyNotes.this, DisplayNote.class);
+                        singleActivity.putExtra("noteID", noteID);
+                        startActivity(singleActivity);
+                    }
+                });
+
+            }
+        };
+        recyclerView.setAdapter(FBRA);
+    }
+
+    public static class NoteViewHolder extends RecyclerView.ViewHolder {
+        View view;
+
+        public NoteViewHolder(View itemView) {
+            super(itemView);
+            view = itemView;
+        }
+
+        public void setTitle(String title) {
+            TextView noteTitle = view.findViewById(R.id.txtnamerow);
+            noteTitle.setText(title);
+        }
+
+        public void setTime(String time) {
+            TextView noteTime = view.findViewById(R.id.txtdate);
+            noteTime.setText(time);
+        }
+
     }
 
     @Override
